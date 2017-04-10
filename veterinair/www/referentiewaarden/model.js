344bc4cd95b1052abb3b05e56ebebbc26c646c4c
Viola van Leeuwen <viola.van.leeuwen@qps.nl>
2017-04-10 20:11:10 +0200

    Eerste versie nieuwe referentiewaarden app ingevoegd; model moet nog goed ingevuld worden en styling verbeteren

---
+++ veterinair/www/referentiewaarden/model.js
@@ -0,0 +1,38 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+		micromol = '\u03BCmol';
+	    $scope.lists = [
+		{name: 'kat',
+		list:[
+			{name: 'klinische chemie',
+		     values: [{name: 'Ureum',value: '8,1-12,8',unit: micromol},
+			          {name: 'Kreatinine',value: '76-164',unit: 'mmol'},
+					  {name: 'Glucose (nuchter)',value: '3,4-5,7',unit: 'mmol'}]
+		    },
+			{name: 'eiwitten',
+			 values: [{name: 'Totaal eiwit', value:'54-70',unit:'g/L'},
+			          {name: 'Albumine', value:'25-34',unit:'g/L'}]
+			}]},
+		{name: 'hond',
+		list:[
+			{name: 'klinische chemie',
+		     values: [{name: 'Ureum',value: '8,1-13',unit: 'mmol'},
+			          {name: 'Kreatinine',value: '86-164',unit: 'mmol'},
+					  {name: 'Glucose (nuchter)',value: '5,4-5,7',unit: 'mmol'}]
+		    },
+			{name: 'eiwitten',
+			 values: [{name: 'Totaal eiwit', value:'54-80',unit:'g/L'},
+			          {name: 'Albumine', value:'25-74',unit:'g/L'}]
+			}]},
+		{name:'vogels',
+		list:[
+			{name:'ostrich',
+			 values: [{name: 'Urea', value:'0,5-0,8',unit:'mmol/L'},
+			         {name: 'Creatinine', value: '/',unit:'mmol/L'}]
+			},
+			{name:'Peregrine falcon',
+			values: [{name: 'Urea', value:'0,8-2,9',unit:'mmol/L'},
+			         {name: 'Creatinine', value: '24-84', unit: 'mmol/L'}]
+			}]}
+		]
+});
